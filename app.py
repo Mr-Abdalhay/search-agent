ccca3c760493dcaa8113a90a8f643fde1301da5c
Mr.Abdalhay Abass <abassabdalhay@gmail.com>
2025-11-05 12:27:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,3 @@
-# from flask import Flask, render_template, request
-# from news_search_agent import NewsSearchAgent
-# import config
-
-# app = Flask(__name__)
-
-# # Instantiate the NewsSearchAgent with your API key
-# agent = NewsSearchAgent(api_key=config.NEWSAPI_KEY)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/search', methods=['POST'])
-# def search():
-#     query = request.form['query']
-#     days_back = int(request.form['days_back'])
-    
-#     results = agent.search_news(query, days_back=days_back)
-    
-#     return render_template('results.html', results=results, query=query)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
 from flask import Flask, render_template, request
 from news_search_agent import NewsSearchAgent
 from gemini_summarizer import GeminiSummarizer # 1. Import the new class
@@ -84,3 +60,4 @@ def search():
     
 if __name__ == "__main__":
     app.run(debug=True)
+
